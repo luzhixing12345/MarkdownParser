428ddfbb7df549e408687b016f8d98fdbb338a04
luzhixing12345 <luzhixing12345@163.com>
2023-12-03 11:32:51 +0800

    [test] : compare html and md to make sure stable

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
 import MarkdownParser
 import unittest
-
+import os
 
 class TestMyMdParser(unittest.TestCase):
     def test_parse_heading(self):
 
-        test_id = 21
+        test_id = 22
+        md_root_path = f'./testfiles/md'
+        html_root_path = f'./testfiles/html'
         MarkdownParser.parse("")
         MarkdownParser.parse("# Heading")
         MarkdownParser.parse_toc("")
 
         for i in range(1, test_id + 1):
-            MarkdownParser.parse_file(f"./testfiles/test{i}.md")
-            MarkdownParser.parse_file_toc(f"./testfiles/test{i}.md")
+            md_path = os.path.join(md_root_path, f"test{i}.md")
+            html_path = os.path.join(html_root_path, f"test{i}.html")
+            MarkdownParser.parse_file(md_path)
+            html = MarkdownParser.parse_file_toc(md_path)
+            with open(html_path, 'r', encoding='utf-8') as f:
+                self.assertEqual(html, f.read())
 
 
 if __name__ == "__main__":
